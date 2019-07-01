99db59686cf3273d7a0d0a63768fe86de53d9455
Jesse Katsumata <jessekatsumata@jesses-mbp.lan>
2019-07-02 08:56:34 +0900

    :wrench: add check for .zshrc

--- set_zshrc.sh
+++ set_zshrc.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-exec cp ~/.zshrc ~/.zshrc.backup
+#!/bin/bash
+ZSHRC="~/.zshrc"
+if [ -e $ZSHRC ]; then
+  echo ".zshrc found. Creating backup..."
+  exec cp ~/.zshrc ~/.zshrc.backup
+fi
 exec cp .zshrc ~/.zshrc 
+echo "Successfully copied .zshrc"
