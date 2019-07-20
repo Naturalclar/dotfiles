a8db4507bf451921906a44402adaa1fc6155b7ef
jesse <jesse.katsumata@cureapp.jp>
2019-07-20 17:29:25 +0900

    :sparkles: change KeyRepeat setting

--- install_shell.sh
+++ install_shell.sh
@@ -3,3 +3,7 @@ exec sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zs
 
 # Clone zsh auto suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+# set default key repeat
+defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
