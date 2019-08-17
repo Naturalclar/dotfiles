8b958233503034ca499cbb53244aaa342f0924a5
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-08-17 11:35:25 +0900

    :fire: remove unnecessary scripts

--- set_hyper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HYPER="~/.hyper.js"
-if [ -e $HYPER ]; then
-  echo ".hyper.js found. Creating backup..."
-  exec cp ~/.hyper.js ~/.hyper.js.backup
-fi
-exec cp .hyper.js ~/.hyper.js 
-echo "Successfully copied .hyper.js"
