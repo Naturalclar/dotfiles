4c452bb5dc8e48a80f4edff5afa8040cae34f5f2
Jesse Katsumata <jesse.katsumata@gmail.com>
2023-12-18 16:55:45 +0900

    add more git aliases (#45)
    * add git aliases
    
    * add gh alias
    
    * add more git alias
    
    * allow args to gpcb

--- windows/alias.ps1
+++ windows/alias.ps1
@@ -12,3 +12,41 @@ Set-Alias -Name g -Value git
 Function gst {
     git status    
 }
+Function gaa {
+    git add --all
+}
+Function gcmm {
+    git commit -m $args
+}
+Function gpull {
+    git pull origin $args
+}
+Function get_current_branch {
+    git branch --show-current
+}
+Function gpcb {
+    git push origin $(get_current_branch) $args
+}
+Function gcb {
+    git switch -c $args
+}
+Function gco {
+    git switch $args
+}
+Function get_default_branch {
+    git remote show origin | Select-String "HEAD branch" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+}
+Function gcod {
+    # git switch to default branch
+    git switch $(get_default_branch)
+}
+Function gsu {
+    # git stash
+    git stash -u
+}
+
+# Add Alias for gh cli commands
+Function ghview {
+    # Open the current repository in browser
+    gh repo view -w
+}
