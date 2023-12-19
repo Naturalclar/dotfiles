84c0c10ad6a1c216b3269102ed08bbcd77a639b8
Naturalclar <jesse.katsumata@gmail.com>
2023-12-19 14:50:07 +0900

    feat: add missing direnv config

--- windows/path.ps1
+++ windows/path.ps1
@@ -1,6 +1,10 @@
 # Add direnv config path
 Invoke-Expression "$(direnv hook pwsh)"
 
+$env:DIRENV_CONFIG = $env:APPDATA + '\direnv\config'
+$env:XDG_CACHE_HOME = $env:APPDATA + '\direnv\cache'
+$env:XDG_DATA_HOME = $env:APPDATA + '\direnv\data'
+
 # Add pypoetry to path
 $poetryPath = ';' + $env:APPDATA + '\pypoetry\venv\Scripts'
 
