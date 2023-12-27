191a3ce705b7368d9751c8b403a57d7459784e1f
Jesse Katsumata <jesse.katsumata@gmail.com>
2023-12-27 14:14:58 +0900

    change color of terminal if there are changes

--- windows/terminal.ps1
+++ windows/terminal.ps1
@@ -3,6 +3,14 @@ function prompt {
     # Get name of the current directory
     $directoryName = (Get-Location).Path.Split("\")[-1]
     $branch = $(get_current_branch)
-    Write-Host "$directoryName [$branch] " -NoNewline -ForegroundColor Green
+
+    # Check if there are any changes in the current directory without showing them
+    $has_diff = $(git status --porcelain > $null; Write-Output $?)
+
+    $ForegroundColor = "Green"
+    if ($has_diff -eq "True") {
+        $ForegroundColor = "Red"
+    }
+    Write-Host "$directoryName [$branch] " -NoNewline -ForegroundColor $ForegroundColor
     return "PS> "
 }
