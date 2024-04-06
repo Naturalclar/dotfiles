8b947941227d776d785483a07e792aa118405fcc
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-04-06 10:10:08 +0900

    feat: setup nvim for windows psh (#72)

---
+++ docs/windows.md
@@ -0,0 +1,17 @@
+# Windows Memo
+
+## Installing dependencies for telescope
+
+### Installing C Compiler on Windows
+
+```bash
+scoop install mingw
+```
+
+### Installing fzf on Windows
+
+````bash
+```bash
+scoop install fzf
+```
+````
--- setup-nvim.ps1
+++ setup-nvim.ps1
@@ -1,15 +1,6 @@
-# create directory for init.vim if it doesn't exist
+# create symbolic link for nvim config directory
 
 $nvimDir = "$env:USERPROFILE\AppData\Local\nvim"
 
-if (-not (Test-Path $nvimDir)) {
-    New-Item -ItemType Directory -Path $nvimDir
-}
+New-Item -ItemType SymbolicLink -Path $nvimDir -Value $PSScriptRoot\.config\nvim
 
-# create symbolic link to init.vim
-
-$initVim = "$nvimDir\init.vim"
-
-if (-not (Test-Path $initVim)) {
-    New-Item -ItemType SymbolicLink -Path $initVim -Value $PSScriptRoot\.config\nvim\init.vim
-}
