8c45931f263c4f4f55ff643162eddbfdcc32cfd8
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-04-08 13:28:17 +0900

    feat: set default directory on duplicate

--- windows/terminal.ps1
+++ windows/terminal.ps1
@@ -1,22 +1,30 @@
+setx PROMPT "%PROMPT%"
 # Set the shell to display current branch name
 function prompt {
-    # Get name of the current directory
-    $directoryName = (Get-Location).Path.Split("\")[-1]
-    $branch = $(get_current_branch)
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
 
-    # Check if there are any changes in the current directory without showing them
-    $has_diff = $null -ne $(git status --porcelain)
-    if ($has_diff -eq "True") {
-        $branchColor = "Red"
-    }
-    else {
-        $branchColor = "Green"
-    }
-    Write-Host "╭─ " -NoNewline -ForegroundColor "DarkGreen"
-    Write-Host "$directoryName " -NoNewline -ForegroundColor "Magenta" 
-    Write-Host "[$branch] " -ForegroundColor $branchColor
-    Write-Host "╰─$ " -NoNewline -ForegroundColor "DarkGreen"
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS> ";
+  # Get name of the current directory
+  $directoryName = (Get-Location).Path.Split("\")[-1]
+  $branch = $(get_current_branch)
 
-    return "PS> "
+  # Check if there are any changes in the current directory without showing them
+  $has_diff = $null -ne $(git status --porcelain)
+  if ($has_diff -eq "True") {
+      $branchColor = "Red"
+  }
+  else {
+      $branchColor = "Green"
+  }
+  Write-Host "╭─ " -NoNewline -ForegroundColor "DarkGreen"
+  Write-Host "$directoryName " -NoNewline -ForegroundColor "Magenta" 
+  Write-Host "[$branch] " -ForegroundColor $branchColor
+  Write-Host "╰─$ " -NoNewline -ForegroundColor "DarkGreen"
+
+  return $out
 }
 
