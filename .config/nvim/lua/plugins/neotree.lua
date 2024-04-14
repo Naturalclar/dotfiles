33ea316fa5b04e27c7d1a45df6adc655dafa9e4a
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-04-14 21:20:15 +0900

    feat: close neotree on open

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -13,6 +13,14 @@ return {
           visible = true,
         },
       },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
     })
   end,
 }
