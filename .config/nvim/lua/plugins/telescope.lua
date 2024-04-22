72071664530febefd4166c2130b7414e952fbe02
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-04-22 12:26:35 +0900

    setup telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,8 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+  end,
+}
