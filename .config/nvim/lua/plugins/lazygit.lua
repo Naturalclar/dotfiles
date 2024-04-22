6fce43aa2ab0e6799c9502c0bb83d809759bd88e
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-04-22 13:56:25 +0900

    fix/telescopesetup (#97)
    * setup telescope
    
    * add key maps for tmux like new pane
    
    * run zsh on bang command
    
    * fix shell

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -11,9 +11,4 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-  },
 }
