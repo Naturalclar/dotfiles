2c199facd347e0bf7ca63806cbcf4c3f28e5ba24
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-04-22 13:31:48 +0900

    add key maps for tmux like new pane

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+map("n", "<leader>%", "<C-W>v", { desc = "Split Window Right" })
+map("n", '<leader>"', "<C-W>s", { desc = "Split Window Down" })
