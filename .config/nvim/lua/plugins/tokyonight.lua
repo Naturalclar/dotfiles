4a742700a5f3c32f6ab0c1ba92b80bfddf826830
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-05-20 16:05:36 +0900

    feat: set neovim background to transparent

---
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    theme = "night",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
