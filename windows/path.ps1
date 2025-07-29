6cab09ae5332a64b04c6027d1c5817af58594a2c
Jesse Katsumata <jesse.katsumata@gmail.com>
2025-07-29 18:46:24 +0900

    fix: set git bash path

--- windows/path.ps1
+++ windows/path.ps1
@@ -1,6 +1,8 @@
 # Add direnv config path
 Invoke-Expression "$(direnv hook pwsh)"
 
+$env:CLAUDE_CODE_GIT_BASH_PATH = "$env:USERPROFILE\scoop\shims\bash.exe"
+
 $env:DIRENV_CONFIG = $env:APPDATA + '\direnv\config'
 $env:XDG_CACHE_HOME = $env:APPDATA + '\direnv\cache'
 $env:XDG_DATA_HOME = $env:APPDATA + '\direnv\data'
